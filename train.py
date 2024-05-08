d4ab2a55768f14724eb7319679d63f65702d00cf
JSansa <joansansabernat@gmail.com>
2024-05-08 11:16:28 +0100

    Update train.py

--- train.py
+++ train.py
@@ -10,25 +10,28 @@ def ingest_data(filepath: str) -> pd.DataFrame:
 
 def clean_data(df: pd.DataFrame) -> pd.DataFrame:
     # suppression des lignes avec des valeurs manquantes
-    df = df.dropna(axis=8)
+    df = df[['survuved', 'pclass', 'sex', 'age']]
+    df.dropna(inplace=True)
+
     # remplacer les valeurs non numériques par des valeurs numériques
     df['sex'].replace(to_replace=['male', 'female'], value=[0, 1], inplace=True)
     return df
 
 def train_model(df: pd.DataFrame) -> ClassifierMixin:
     # instantiate model
-    model = KNeighborsClassifier()
+    model = KNeighborsClassifier(4)
 
     # split data
     y = df['survived']
     X = df.drop('survived', axis=1)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.28)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
     # train model
     model.fit(X_train, y_train)
 
     # evaluate model
-    model.score(X_test, y_test)
+    score = model.score(X_test, y_test)
+    print(f"Model score: {score}")
 
     return model
 
